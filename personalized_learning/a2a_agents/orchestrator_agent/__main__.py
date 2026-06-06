20a54615beb4f3773ea1debf828edc3b9eb3bc23
Dina Bavli <39160989+dinbav@users.noreply.github.com>
2026-06-06 21:17:58 +0300

    Delete personalized_learning directory

--- personalized_learning/a2a_agents/orchestrator_agent/__main__.py
+++
@@ -1,6 +0,0 @@
-from a2a_agents.server_factory import run
-from a2a_agents.orchestrator_agent.agent_card import public_agent_card
-from a2a_agents.orchestrator_agent.agent_executor import AgentExecutor
-
-if __name__ == "__main__":
-    run(public_agent_card, AgentExecutor, port=8090)
--- personalized_learning/a2a_agents/remote_agents/topic_explainer_agent/__main__.py
+++
@@ -1,6 +0,0 @@
-from a2a_agents.server_factory import run
-from .agent_card import public_agent_card
-from .agent_executor import AgentExecutor
-
-if __name__ == "__main__":
-    run(public_agent_card, AgentExecutor, port=8091)
